b13c944a312a1b6e8d7c7cc8ad7b46e18833200e
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-08 08:54:40 +0900

    first time setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopBox v-bind:name="name"/>
+    <div id="contentWrap">
+        <ContentBox v-bind:name="name"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TopBox from './components/TopBox.vue'
+    import ContentBox from "@/components/contentBox/ContentBox";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            TopBox,
+            ContentBox
+        },
+        data() {
+            return{
+                name: "김잭슨"
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        margin: 0px;
+        background: #F2C6C2;
+    }
+    input{
+        background: #F2C6C2;
+        border: none;
+        border-radius: 20px;
+        color: #86A69D;
+        height: 25px;
+        padding: 0px 10px 0px 10px;
+    }
+    hr{
+        border-color: #F28585;
+        border-width: thin;
+    }
+    #header {
+        background: #F2E8DF;
+        box-shadow: 0px 0.05px 5px 0px #F28585;
+    }
+    #contentWrap {
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
