4a4d4ba803842d70b1b8fce6997103738a976135
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-09 10:44:24 +0900

    make write popup base(date formatting)

--- src/components/contentBox/PopWrite.vue
+++ src/components/contentBox/PopWrite.vue
@@ -6,14 +6,20 @@
 
             <div class="header borderBtm">
                 <h1>게시물 작성하기</h1>
-                <img @click="$emit('closePop')" id="close" src="https://img.icons8.com/color/512/close-window.png" alt="닫기버튼" />
+                <img @click="$emit('closePop')" id="close" src="https://img.icons8.com/color/512/close-window.png"
+                     alt="닫기버튼"/>
             </div>
             <div class="content">
                 <div class="writer">
-
+                    <img class="profile" src="https://img.icons8.com/pastel-glyph/2x/gender-neutral-user.png"
+                         alt="profile"/>
+                    <div>
+                        <h3>{{user}}</h3>
+                        <p>{{thisTime}}</p>
+                    </div>
                 </div>
                 <div class="write">
-                    <input type="text" style="background: #F2B263; width: 97%; height: 400px;"/>
+                    <textarea :value="userName" id="writeInput"></textarea> <!-- 개행을 위한 태그 -->
                 </div>
             </div>
             <div class="button">
@@ -30,7 +36,66 @@
     export default {
         name: "PopWrite",
         props: {
-            popCheck: Boolean
+            popCheck: Boolean,
+            userName: String
+        },
+        data() {
+            return {
+                user: this.userName.replace("님, 무슨 생각을 하고 계신가요?", ""),
+                thisTime: new Date()
+            }
+        },
+        created (){ //DOM이 생성되기 전에 메서드 실행
+            this.thisTime=this.formatOfTime(this.thisTime)
+        },
+        methods: {
+            /*
+                작업자: 함정희
+                input: Date Type 변수
+                output: YYYY/MM/DD HH24:Mi
+                기능: 인자로 제공된 변수을 format된 문자열로 반환
+            */
+            formatOfTime: function (thisTime) {
+                let fmt; // thisTime formatting
+                let morethan = 0; // function for comparison
+
+                // 년
+                fmt = thisTime.getFullYear() + "/"
+
+                // 월
+                morethan = (thisTime.getMonth() + 1)
+                if (morethan < 10) {
+                    morethan = "0" + morethan
+                }
+                fmt += morethan + "/"
+
+                // 일
+                morethan = thisTime.getDate()
+                if (morethan < 10) {
+                    morethan = "0" + morethan
+                }
+                fmt += morethan + " "
+
+                //시
+                morethan = thisTime.getHours()
+                if (morethan < 10) {
+                    morethan = "0" + morethan
+                }
+                fmt += morethan + ":"
+
+                //분
+                morethan = thisTime.getMinutes()
+                if (morethan < 10) {
+                    morethan = "0" + morethan
+                }
+                fmt += morethan
+
+                thisTime=fmt
+
+                console.log(this.thisTime)
+
+                return thisTime
+            }
         }
     }
 </script>
@@ -46,43 +111,78 @@
         height: 100%;
         top: 0;
     }
+
     #pop {
         background: #F2B263;
         width: 65%;
     }
-    .header{
+
+    .header {
         display: flex;
         justify-content: center;
         align-items: center;
     }
-    .header > h1{
+
+    .header > h1 {
         width: 90%;
         text-align: center;
     }
-    #close{
+
+    #close {
         width: 50px;
         height: 50px;
     }
-    #close:hover{
+
+    #close:hover {
         cursor: pointer;
     }
-    .button{
+
+    .button {
         display: flex;
         justify-content: center;
         align-items: center;
         padding: 10px 0px 10px 0px;
     }
+
     .button > button {
         border: none;
         border-radius: 10px;
-        width:95%;
+        width: 95%;
         height: 40px;
         background: #86A69D;
     }
+
     .button > button:hover {
         cursor: pointer;
     }
+
     .button > button:active {
         background: #86A09D;
+    }
+
+    .writer {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    .profile {
+        width: 100px;
+        height: 100px;
+    }
+
+    .write {
+        display: flex;
+        justify-content: center;
+    }
+
+    #writeInput {
+        background: #F2B263;
+        width: 97%;
+        height: 400px;
+        border-radius: 0px;
+        border: none;
+        color: #F2E8DF;
+        font-size: 20px;
     }
 </style>
